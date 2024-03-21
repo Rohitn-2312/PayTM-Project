2e904ae6251c0ad4c5ec43e3fe27ed978ea066d0
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-03-21 19:35:49 +0000

    route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,7 @@
-const express=require("express")
+const express=require("express");
+const userRouter=require("./users");
 const router = express.Router();
+
+router.use("/user",userRouter);
+
 module.exports=router;
