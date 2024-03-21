c98a6cf1d370e8b7a429d13907c522b1f8a26d09
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-03-21 19:52:59 +0000

    route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express=require("express")
-
+const cors=require("cors");
 const rootRouter=require("./routes/index");
+
+app.use(cors());
+app.use(express.json());
 
 const app=express();
 
 app.use("/api/v1",rootRouter);
+app.listen(3000);
